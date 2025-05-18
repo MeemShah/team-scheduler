4a678ca371cd202944858b7ae64c80d3d810f558
MostofaShah <shah.meem@exabyting.com>
2025-05-18 14:32:00 +0600

    refactor: update file formate

--- src/api/controller/startup.py
+++ src/api/controller/startup.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI, Request
 from src.api.controller.controller import Controller
 from src.team_scheduler_svc.team_scheduler import TeamScheduler
 from ...config.config import Config
-from ...api.routes import register_routes
 
 def configure_startup(app: FastAPI,cnf: Config , scheduler: TeamScheduler):
     @app.on_event("startup")
     def startup_event():
         controller = Controller(cnf=cnf,team_svc=scheduler)
         app.state.controller = controller
-        register_routes(app)
 
 def get_controller(request: Request) -> Controller:
     return request.app.state.controller
